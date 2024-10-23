175951180f6edb703e6fadd0a2d4561a167ba2f0
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2024-10-23 16:59:20 -0500

    Completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { TaskList } from './components/TaskList/TaskList'
-import { HashRouter as Router, Route, Switch, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom' // Cambié Switch por Routes
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
 import './App.css'
 import { useEffect, useState } from 'react'
 
 const App = () => {
-  const[tasks, setTasks] = useState(()=> {
+  const [tasks, setTasks] = useState(() => {
     const saveTasks = localStorage.getItem('mydayapp-reactjs')
-    return saveTasks ?  JSON.parse(saveTasks) : []
+    return saveTasks ? JSON.parse(saveTasks) : []
   })
 
   useEffect(() => {
@@ -17,13 +17,13 @@ const App = () => {
 
   const addTask = (task) => {
     setTasks(prevTasks => [
-      ...prevTasks, {id: Date.now().toString(), title: task, completed: false}
+      ...prevTasks, { id: Date.now().toString(), title: task, completed: false }
     ])
   }
 
   const editTask = (id, newTitle) => {
     setTasks(prevTasks =>
-      prevTasks.map(task => 
+      prevTasks.map(task =>
         task.id === id ? { ...task, title: newTitle } : task
       )
     )
@@ -35,35 +35,33 @@ const App = () => {
 
   const toggleComplete = (id) => {
     setTasks(prevTasks =>
-      prevTasks.map(task => 
+      prevTasks.map(task =>
         task.id === id ? { ...task, completed: !task.completed } : task
       )
     )
   }
-  
+
   const clearCompleted = () => {
     setTasks(prevTasks => prevTasks.filter(task => !task.completed));
   };
 
   return (
-    <>
-      <div className='app-container'>
-        <div className='fondo'></div>
-        <Router>
-          <Header addTask={addTask}></Header>
-          <div className='main'>
-            <Switchwitch>
-              <Route path='/' element={<Navigate to="/all"/>}/>
-              <Route path='/Gestor-tareas' element={<Navigate to="/all"/>}/>
-              <Route path='/all' element= {<TaskList tasks={tasks} editTask={editTask} deleteTask={deleteTask} toggleComplete={toggleComplete}  filter="all"/>}/>
-              <Route path='/pending' element= {tasks.length > 0 ? <TaskList tasks={tasks} editTask={editTask} deleteTask={deleteTask} toggleComplete={toggleComplete}  filter="pending"/> : <Navigate to="/all"/>}/>
-              <Route path='/completed' element= {tasks.length > 0 ? <TaskList tasks={tasks} editTask={editTask} deleteTask={deleteTask} toggleComplete={toggleComplete}  filter="completed"/> : <Navigate to="/all"/>}/>
-            </Switchwitch>
-            <Footer tasks={tasks} clearCompleted={clearCompleted}></Footer>
-          </div>
-        </Router>
-      </div>
-    </>
+    <div className='app-container'>
+      <div className='fondo'></div>
+      <Router>
+        <Header addTask={addTask} />
+        <div className='main'>
+          <Routes>
+            <Route path='/' element={<Navigate to="/all" />} />
+            <Route path='/Gestor-tareas' element={<Navigate to="/all" />} />
+            <Route path='/all' element={<TaskList tasks={tasks} editTask={editTask} deleteTask={deleteTask} toggleComplete={toggleComplete} filter="all" />} />
+            <Route path='/pending' element={tasks.length > 0 ? <TaskList tasks={tasks} editTask={editTask} deleteTask={deleteTask} toggleComplete={toggleComplete} filter="pending" /> : <Navigate to="/all" />} />
+            <Route path='/completed' element={tasks.length > 0 ? <TaskList tasks={tasks} editTask={editTask} deleteTask={deleteTask} toggleComplete={toggleComplete} filter="completed" /> : <Navigate to="/all" />} />
+          </Routes>
+          <Footer tasks={tasks} clearCompleted={clearCompleted} />
+        </div>
+      </Router>
+    </div>
   )
 }
 
