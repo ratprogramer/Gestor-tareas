216a1eb869136117c901f137ca446ff115a73500
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2024-10-23 14:41:13 -0500

    Ocultar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,31 @@
 import './Footer.css'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 export function Footer({ clearCompleted, tasks }) {
 
+  const incompleteTasks = tasks.filter(task => !task.completed);
+  console.log(incompleteTasks);
+  
   return (
     <>
-      <div>
-        <p>{tasks.length == 1 ? `${tasks.length} item left` : `${tasks.length} items left` }</p>
-      </div>
-      <div>
-        <Link to="/all" >Todos</Link>
-        <Link to="/completed">Completados</Link>
-        <Link to="/pending">Pendientes</Link>
-      </div>
-      <footer>
-        <button onClick={clearCompleted}>Clear completed</button>
-      </footer>
+      {tasks.length > 0 ? 
+        (
+          <>
+              <div>
+              <p> {incompleteTasks.length === 1 ? 
+              ( <> <strong>{incompleteTasks.length}</strong> item left </>) : (
+              <><strong>{incompleteTasks.length}</strong> items left</>)}
+              </p>
+              </div>
+              <div>
+                <Link to="/all" >Todos</Link>
+                <Link to="/completed">Completados</Link>
+                <Link to="/pending">Pendientes</Link>
+              </div>
+              <footer>
+                <button onClick={clearCompleted}>Clear completed</button>
+              </footer>
+          </>) : ( <Navigate to='/all'></Navigate>)
+      }
     </>
   )
 }
